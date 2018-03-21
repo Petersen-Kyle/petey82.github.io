9fc6f91a4d87150ff8475406ac8de28d4278b464
petey82 <35268701+petey82@users.noreply.github.com>
2018-03-20 22:58:31 -0500

    lesson10

--- Sandbox/scripts/active.js
+++ Sandbox/scripts/active.js
@@ -1,7 +1,7 @@
 (function() {
     var nav = document.getElementById('nav'),
         anchor = nav.getElementsByTagName('a'),
-        current = window.location.pathname.split('/')[1];
+        current = window.location.pathname.split('/')[2];
         window.alert(current);
         for (var i = 0; i < anchor.length; i++) {
         if(anchor[i].href == current) {
