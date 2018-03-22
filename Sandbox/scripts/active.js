6513c0870c8828d82c5217c979403c8559a872e6
petey82 <35268701+petey82@users.noreply.github.com>
2018-03-21 19:24:44 -0500

    lesson 10

--- Sandbox/scripts/active.js
+++ Sandbox/scripts/active.js
@@ -1,13 +1,5 @@
-(function setActive() {
-    var nav = document.getElementById('nav'),
-        anchor = nav.getElementsByTagName('a'),
-        current = window.location.pathname.split('/')[2];
-        window.alert(current);
-        window.alert(anchor);
-        for (var i = 0; i < anchor.length; i++) {
-        if(anchor[i].href == current) {
-        	window.alert(anchor);
-            anchor[i].className = "active";
-        }
-    }
-})();
+$(document).ready(function() {
+	// get current URL path and assign 'active' class
+	var pathname = window.location.pathname;
+	$('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+})
