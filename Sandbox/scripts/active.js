3c0df37b68dc96db4cfc6933f2e9b85baadc2bef
petey82 <35268701+petey82@users.noreply.github.com>
2018-03-21 19:32:51 -0500

    10

--- Sandbox/scripts/active.js
+++ Sandbox/scripts/active.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
 	// get current URL path and assign 'active' class
 	var pathname = window.location.pathname;
 	window.alert(pathname);
-	$('nav > li > a[href="/' + location.pathname.split("/")[2] + '"]'.parent().addClass('active'));
+	$('nav > li > a[href="/' + location.pathname.split("/")[3] + '"]'.parent().addClass('active'));
 })
