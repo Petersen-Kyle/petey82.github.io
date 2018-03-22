1f84890d7278df88fea37327a6dfaebf6022d477
petey82 <35268701+petey82@users.noreply.github.com>
2018-03-21 20:09:07 -0500

    10

--- Sandbox/scripts/active.js
+++ Sandbox/scripts/active.js
@@ -1,7 +1,7 @@
 (function() {
     var nav = document.getElementById('nav'),
         anchor = nav.getElementsByTagName('a'),
-        current = window.location.pathname.split('/');
+        current = window.location.pathname;
         window.alert(anchor[0].href);
         window.alert(current[2]);
         for (var i = 0; i < anchor.length; i++) {
