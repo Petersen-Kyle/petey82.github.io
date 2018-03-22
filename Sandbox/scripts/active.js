6b3e13257ef3ec26d1314fc55bc9d011db11d153
petey82 <35268701+petey82@users.noreply.github.com>
2018-03-21 19:58:55 -0500

    10

--- Sandbox/scripts/active.js
+++ Sandbox/scripts/active.js
@@ -2,8 +2,8 @@
     var nav = document.getElementById('nav'),
         anchor = nav.getElementsByTagName('a'),
         current = window.location.pathname.split('/');
-        window.alert(anchor[2]);
-        window.alert(current[9]);
+        window.alert(anchor);
+        window.alert(current[3]);
         for (var i = 0; i < anchor.length; i++) {
         	if(anchor[i].href == current) {
             anchor[i].className = "active";
