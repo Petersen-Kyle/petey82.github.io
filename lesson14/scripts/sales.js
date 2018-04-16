598d6bccab42f5e99caa7ff3dbfef7bf612cc7a5
petey82 <35268701+petey82@users.noreply.github.com>
2018-04-16 13:50:17 -0500

    lesson 14

---
+++ lesson14/scripts/sales.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle("medium");
+    });
+});
+
+$(document).ready(function(){
+    $("#flip2").click(function(){
+        $("#panel2").slideToggle("medium");
+    });
+});
+
+$(document).ready(function(){
+    $("#flip3").click(function(){
+        $("#panel3").slideToggle("medium");
+    });
+});
+
+$(document).ready(function(){
+    $("#close").click(function(){
+        $("#panel").slideToggle("medium");
+    });
+});
+
+$(document).ready(function(){
+    $("#close2").click(function(){
+        $("#panel2").slideToggle("medium");
+    });
+});
+
+$(document).ready(function(){
+    $("#close3").click(function(){
+        $("#panel3").slideToggle("medium");
+    });
+});
